f7bf9c8c81b7d3cf57b9f870a993c5638dd36668
Anjali-Sharmarpr <anjalirpr01@gmail.com>
2023-03-08 22:22:17 +0530

    week 2 assignments on Basics of java - I

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package WeekAssignments;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+               
+		System.out.println("Hello World");
+		System.out.println("Anjali Sharma");
+	}
+
+}
---
+++ Sum.java
@@ -0,0 +1,19 @@
+package WeekAssignments;
+import java.util.Scanner;
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		      Scanner input = new Scanner(System.in);
+
+		      int a = input.nextInt();
+		      int b= input.nextInt();
+
+		      System.out.println("Sum is "+ (a+b));
+		    }
+		}
+
+	
+
+
