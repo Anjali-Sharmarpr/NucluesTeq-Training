54e0bd3c99e51bdb7970529982af3aded9e71628
Anjali-Sharmarpr <anjalirpr01@gmail.com>
2023-04-20 00:39:39 +0530

    Junit Testing Program

---
+++ UnitTesting/src/Testing/Calculator.java
@@ -0,0 +1,25 @@
+package Testing;
+
+
+public class Calculator {
+     
+	public int add(int a,int b) {
+		return a+b;
+	}
+	
+	public int sub(int a , int b) {
+		return a-b;
+	}
+	
+	public int multiply(int a,int b) {
+		return a*b;
+	}
+	
+	public int divide(int a,int b) {
+		return a/b;
+	}
+	
+	public double power(int a,int b) {
+		return Math.pow(a,b);
+	}
+}
---
+++ UnitTesting/src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * @author Nisha
+ *
+ */
+module UnitTesting {
+	requires junit;
+	requires org.junit.jupiter.api;
+}
